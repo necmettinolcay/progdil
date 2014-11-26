4bb76f7695196b1303951f746d1a0190008e6ab4
Olcay Özer <olcay.ozer@bil.omu.edu.tr>
2014-11-26 02:23:53 +0200

    struct ismi değiştirildi

--- src/ReplaceUnderscoreWithSpace.go
+++ src/ReplaceUnderscoreWithSpace.go
@@ -1,33 +1,28 @@
- 
+
   package main
   import "os"
   import "fmt"
   import s "strings"
   import "regexp"
 
- 
-
-  type rect struct {
+  type MyString struct {
 	  arg string
   }
 
- func (a *rect) ReplaceUnderscoreWithSpace() {
-	
-	 fmt.Println(a.arg)
-	
-	r, _ := regexp.Compile("[a-zA-Z0-9]+_+\\w*[^_]")
-	fmt.Println(r.ReplaceAllString(a.arg, s.Replace(r.FindString(a.arg), "_", " " , -1)))
+  func (m *MyString) ReplaceUnderscoreWithSpace() {
+
+	fmt.Println(m.arg)
+        r, _ := regexp.Compile("[a-zA-Z0-9]+_+\\w*[^_]")
+	fmt.Println(r.ReplaceAllString(m.arg, s.Replace(r.FindString(m.arg), "_", " " , -1)))
 
  }
- 
-
-
 
   func main() {
 
-	  arg := rect{arg: os.Args[1]}
-	  arg.ReplaceUnderscoreWithSpace()
+	arg:= MyString{arg: os.Args[1]}
+	arg.ReplaceUnderscoreWithSpace()
 
-  }
+ }
+
 
 
