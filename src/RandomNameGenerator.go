0931ec395f62d2cb11a205b6103203eeb2f64b31
necmettinolcay <olcay.ozer@bil.omu.edu.tr>
2014-11-30 06:00:15 +0200

    createPath oluşturuldu

--- src/RandomNameGenerator.go
+++ src/RandomNameGenerator.go
@@ -1,15 +1,16 @@
-
   package main
-  import "flag"
-  import "fmt"
-  import "math/rand"
-  import "time"
-  import "strings"
-
+  import (
+           "flag"
+	   "fmt"
+	   "math/rand"
+	   "time"
+	   "strings"
+	   "sort"
+	   "os"
+         )
 
   var langPtr *string = flag.String("lang", "tr", "a language")
   var numbPtr *int = flag.Int("numb", 5, "an total")
-  var elements = make(map[string]string)
   var dict = map[string]map[string][]string {
 		"en": {
 			"name": {
@@ -66,62 +67,95 @@
 	}
 
   type generator interface {
-
-	  nameGenerator() string
-	  adjectiveGenerator() string
-
+      nameGenerator() string
+      adjectiveGenerator() string
   }
 
   type lang struct {
-
-	  name, adjective []string
-
+      name, adjective []string
   }
 
   func (s lang) nameGenerator() string {
-
-	  return s.name[randomNumber(len(dict[*langPtr]["name"]))]
-
+      return s.name[randomNumber(len(dict[*langPtr]["name"]))]
   }
 
   func (s lang) adjectiveGenerator() string {
-
-	  return s.adjective[randomNumber(len(dict[*langPtr]["adjective"]))]
-
+      return s.adjective[randomNumber(len(dict[*langPtr]["adjective"]))]
   }
 
   func elementsAdd(g generator) {
+      elements := []string {}
+      path :=  createPath()
+      dirNames, _ := readDir(path)
 
-	  for *numbPtr > 0 {
-		  projectName := strings.Join([]string{g.adjectiveGenerator(),g.nameGenerator()}, " " )
-		  if  _, ok := elements[projectName]; !ok {
-			  elements[projectName] = "exist"
-			  *numbPtr --
-		  }
+      for *numbPtr > 0 {
+          projectName := strings.Join([]string{g.adjectiveGenerator(),g.nameGenerator()}, " " )
+	  existPname := controlName(elements, projectName)
+	  existDir := controlName(dirNames, projectName)
+
+	  if  existPname == false && existDir == false {
+	      elements = append(elements, projectName)
+	      *numbPtr --
 	  }
+      }
+
+          show(elements)
+	  createDir(path, elements)
   }
 
-  func show() {
+  func show(list []string) {
+      for _, pName := range list {
+          fmt.Println(pName)
+      }
+  }
 
-	  for pName, _ := range elements {
-		  fmt.Println(pName)
-	  }
+  func readDir(dirname string) ([]string, error) {
+      f, err := os.Open(dirname)
+      if err != nil {
+          return nil, err
+      }
 
+      names, err := f.Readdirnames(-1)
+      f.Close()
+
+      if err != nil {
+          return nil, err
+      }
+
+      sort.Strings(names)
+      return names, nil
+  }
+
+  func createDir(path string, list []string) {
+      for _, pName := range list {
+          os.Mkdir(path + pName, 0777)
+      }
+
+  }
+
+  func createPath() string {
+      return "../ProjectNames/" + *langPtr + "/"
+  }
+
+ func controlName (list []string, pName string) bool {
+     sort.Strings(list)
+     i := sort.SearchStrings(list, pName)
+
+     if  i < len(list) && list[i] == pName {
+         return true
+     } else {
+           return false
+     }
   }
 
   func randomNumber(limit int) int {
-
-	  rand.Seed(time.Now().UnixNano())
-	  return rand.Intn(limit)
-
-  }
+      rand.Seed(time.Now().UnixNano())
+      return rand.Intn(limit)
+      }
 
   func main() {
-
-	  flag.Parse() //Bayrakların değerlerini öğrenmek için Parse() metodu kullanıldı . ör: --lang = en
-          selectedLang := lang{name: dict[*langPtr]["name"], adjective: dict[*langPtr]["adjective"]}
-	  elementsAdd(selectedLang)
-          show()
-
+      flag.Parse() //Bayrakların değerlerini öğrenmek için Parse() metodu kullanıldı . ör: --lang = en
+      selectedLang := lang{name: dict[*langPtr]["name"], adjective: dict[*langPtr]["adjective"]}
+      elementsAdd(selectedLang)
   }
 
